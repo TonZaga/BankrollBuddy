6fd088e5c1b03e187c0685b597a445adec9538a1
TonZaga <anthony.branham1@gmail.com>
2021-10-31 21:48:58 -0400

    Condensed classes into Program class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace BankrollBuddy
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string title = @"
 
@@ -23,8 +23,49 @@ namespace BankrollBuddy
 
 ";
             Console.WriteLine(title);
-            Gather.getInfo();
-            Console.ReadLine();
+            getInfo();
+            MainMenu();
+        }
+        public static void getInfo()
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your starting bankroll?");
+            var starting = Console.ReadLine();
+            try
+            {
+                int bankroll = Convert.ToInt32(starting);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Amount that you entered is not valid.  Please try again.");
+                getInfo();
+
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Bankroll Buddy menu");
+            Console.WriteLine("\r\nChoose an option:");
+            Console.WriteLine("1) Reverse String");
+            Console.WriteLine("2) Remove Whitespace");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                  //ReverseString();
+                    return true;
+                case "2":
+                    //RemoveWhitespace();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
         }
     }
 }
